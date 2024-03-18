6814b2f7bcb1229cb8cd9e1c013745e446fa812a
rajeshwari <01fe22bca247@kletech.ac.in>
2024-03-18 20:47:09 +0530

    extended code

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
-class Calculator {
+class Calculator extends ExtendedCalc {
+     
     float addition(float operand_1, float operand_2) {
         return (operand_1 + operand_2);
     }
@@ -17,6 +18,10 @@ class Calculator {
         System.out.println("Addition result: " + add_result);
         float sub_result = calc.subtraction(operand_1, operand_2); // corrected
         System.out.println("Subtraction result: " + sub_result);
+        float squ_result = calc.square(operand_1);
+        System.out.println("square result: " + squ_result);
+        float cube_result = calc.cube(operand_1); // corrected
+        System.out.println("cube result: " + cube_result);
     }
 }
 
---
+++ ExtendedCalc.java
@@ -0,0 +1,9 @@
+public class ExtendedCalc {
+
+    float square( float operand_1 ){
+        return operand_1*operand_1;
+    }
+    float cube( float operand_1 ){
+        return operand_1*operand_1*operand_1 ;
+}
+}
